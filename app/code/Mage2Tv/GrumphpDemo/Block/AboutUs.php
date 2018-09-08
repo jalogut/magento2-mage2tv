7e915450c38191fcc692db52eaf265e01401f7cd
alojua <juan.alonso@staempfli.com>
2018-09-08 21:48:43 +0200

    Adding demo module

---
+++ app/code/Mage2Tv/GrumphpDemo/Block/AboutUs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * AboutUs
+ *
+ * @copyright Copyright © 2017 Staempfli AG. All rights reserved.
+ * @author    juan.alonso@gmail.com
+ */
+
+namespace Mage2Tv\GrumphpDemo\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class AboutUs extends Template
+{
+    /**
+     * @var string $_template
+     */
+    protected $_template = "Mage2Tv_GrumphpDemo::aboutus.phtml";
+
+    // write your methods here...
+}
