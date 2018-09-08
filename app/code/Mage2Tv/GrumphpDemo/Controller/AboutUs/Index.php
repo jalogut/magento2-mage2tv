ddbb5d0002714389c20a5c635cd21a43f5c573bc
alojua <juan.alonso@staempfli.com>
2018-09-08 21:47:10 +0200

    add demo module

---
+++ app/code/Mage2Tv/GrumphpDemo/Controller/AboutUs/Index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Index
+ *
+ * @copyright Copyright © 2017 Staempfli AG. All rights reserved.
+ * @author    juan.alonso@gmail.com
+ */
+ 
+namespace Mage2Tv\GrumphpDemo\Controller\AboutUs;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
+class Index extends Action
+{
+    /**
+     * Page Factory variable
+     *
+     * @var PageFactory
+     */
+    protected $pageFactory;
+
+    /**
+     * Construct
+     *
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Index Action
+     * 
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->pageFactory->create();
+        return $resultPage;
+    }
+}
